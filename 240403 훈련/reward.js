b519c8612856d71ce36bdb2366f49114f9bfac3f
Songehyun <songehyun00@gmail.com>
2024-05-02 18:24:40 +0900

    오늘 깃 처음으로 푸쉬를 해봅니다.

---
+++ 240403 훈련/reward.js
@@ -0,0 +1,10 @@
+var names = ['원하는 저녁 먹기', '원하는 점심 먹기', '아침에 10분 더자기', '식사 비용 5000원 더하기' ,'원하는 보상 받기', '1시간 취미 생활', '1시간 운동', '10분 휴식'];
+var random_index1 = Math.floor(Math.random() * names.length);
+var random_index2 = Math.floor(Math.random() * names.length);
+var random_index3 = Math.floor(Math.random() * names.length);
+var random_index4 = Math.floor(Math.random() * names.length);
+var random_re1 = names[random_index1];
+var random_re2 = names[random_index2];
+var random_re3 = names[random_index3];
+var random_re4 = names[random_index4];
+console.log(random_re);
